8c60609df418f9ba42d60464660c9503efe45617
osumico <dev01.io.sem@gmail.com>
2022-06-19 01:50:40 +0300

    create User.js service init

--- server/service/User.js
+++ server/service/User.js
@@ -1,5 +1,23 @@
-class User {
+const UserModel = require("../models/User");
+const bcrypt = require("bcryptjs");
+const uuid = require("uuid");
+const mail = require("./Mail");
 
+class User {
+    async registration(email, password) {
+        const candidate = await UserModel.findOne({ email });
+        if (candidate) {
+            throw new Error(`User with ${ email } -- is exist!`);
+        }
+
+        const hashPass = await bcrypt.hashSync(bcrypt, bcrypt.genSaltSync(6));
+        const emailLink = uuid.v4();
+        const user = await UserModel.create({ 
+            email: email,
+            password: hashPass,
+            emailLink: emailLink
+        });
+    }
 }
 
 module.exports = new User();
