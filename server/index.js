8568895726d7b5480e133e607d74b25284eee0ca
osumico <dev01.io.sem@gmail.com>
2022-06-18 23:40:17 +0300

    default config, next i hide this from git

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const config = require("config");
+
+const app = express();
+const port = process.env.PORT || config.get("port") || 3000;
+
+const start = async() => {
+    try {
+        app.listen(port, callback => {
+            console.log(`APP run at: localhost:${port}`);
+        })
+
+    } catch (e) {
+        console.error(e);
+
+    }
+}
+
+start();
